2d26eba8c1db8c87b217b4afddb66a6bbea7b77d
Katz Sakai <sakai@onflow.jp>
2017-04-22 22:55:47 +0900

    add Myrica package

---
+++ Myrica/tools/chocolateyinstall.ps1
@@ -0,0 +1,38 @@
+﻿function Get-CurrentDirectory
+{
+  $thisName = $MyInvocation.MyCommand.Name
+  [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
+}
+
+$fontHelpersPath = (Join-Path (Get-CurrentDirectory) 'FontHelpers.ps1')
+. $fontHelpersPath
+
+$fontUrl = 'https://github.com/tomokuni/Myrica/archive/2.006.20150301.zip'
+$checksumType = 'sha256';
+$checksum = 'AC85D476A7A8CC809BE015B9593AFFF2D998E7CEA3B9FD9AEE7D9D9A05BA449B';
+
+$destination = Join-Path $Env:Temp 'Myrica'
+
+Install-ChocolateyZipPackage -PackageName 'Myrica' -url $fontUrl -unzipLocation $destination -ChecksumType "$checksumType" -Checksum "$checksum"
+
+$shell = New-Object -ComObject Shell.Application
+$fontsFolder = $shell.Namespace(0x14)
+
+$fontFiles = Get-ChildItem $destination -Recurse -Filter *.TTC
+
+# unfortunately the font install process totally ignores shell flags :(
+# http://social.technet.microsoft.com/Forums/en-IE/winserverpowershell/thread/fcc98ba5-6ce4-466b-a927-bb2cc3851b59
+# so resort to a nasty hack of compiling some C#, and running as admin instead of just using CopyHere(file, options)
+$commands = $fontFiles |
+% { Join-Path $fontsFolder.Self.Path $_.Name } |
+? { Test-Path $_ } |
+% { "Remove-SingleFont '$_' -Force;" }
+
+# http://blogs.technet.com/b/deploymentguys/archive/2010/12/04/adding-and-removing-fonts-with-windows-powershell.aspx
+$fontFiles |
+% { $commands += "Add-SingleFont '$($_.FullName)';" }
+
+$toExecute = ". $fontHelpersPath;" + ($commands -join ';')
+Start-ChocolateyProcessAsAdmin $toExecute
+
+Remove-Item $destination -Recurse
